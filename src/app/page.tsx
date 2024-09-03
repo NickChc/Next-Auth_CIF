2563d89071fc5a0a95dbc21976ef5cc3219b67f3
unknown <nika.chichua26@gmail.com>
2024-09-03 13:19:34 +0400

    feat: auth social providers added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
+import { getSession } from "next-auth/react";
+
+export default async function Home() {
+  // const session = await getSession();
+
+  // if (session?.user) {
+  //   return <h1>Hello, {session.user.email}</h1>;
+  // }
+
   return <div>Home Page</div>;
 }
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,8 +1,10 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import prisma from "../../prisma/prisma";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google, Github],
 });
